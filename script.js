b99410e7454eff1b5998ba65b23458dbea49f64d
Mashi0629 <poboshiniomashi@gmail.com>
2025-06-28 22:25:59 +0530

    questions are included
    not working 👎

---
+++ script.js
@@ -0,0 +1,42 @@
+const questions = [
+    {
+        question:"What does CPU stand for?" ,
+        answers:[
+            {text:"Computer Personal Unit", correct:false},
+            {text:"Central Processing Unit", correct:true},
+            {text:"Central Power Unit", correct:false},
+            {text:"Core Processing Utility", correct:false},
+
+        ]
+    },
+    {
+       question:"Which of the following is an example of an Operating System?" ,
+        answers:[
+            {text:"Linux", correct:true},
+            {text:"Microsoft Excel", correct:false},
+            {text:"Adobe Photoshop", correct:false},
+            {text:"Google Chrome", correct:false},
+
+        ] 
+    },
+    {
+      question:"What is the primary purpose of RAM in a computer?" ,
+        answers:[
+            {text:"Store permanent files", correct:false},
+            {text:" Boost internet speed", correct:false},
+            {text:"Run applications temporarily", correct:true},
+            {text:"Process graphics", correct:false},
+
+        ]   
+    },
+    {
+      question:"Which language is mainly used for web development?" ,
+        answers:[
+            {text:"Python", correct:false},
+            {text:"C++", correct:false},
+            {text:"Java", correct:false},
+            {text:"JavaScript", correct:true},
+
+        ]   
+    }
+]
